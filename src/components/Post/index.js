254d5c34b45abf0f33fc386205bb73ef6d3d8fb4
Chau Tran <cctran96@gmail.com>
2021-09-12 19:12:58 -0700

    post edits

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
+import { useLocation, useHistory } from "react-router-dom"
+import { ImArrowUp, ImArrowDown } from "react-icons/im"
 import Loading from "../Misc/Loading"
 import Error from "../Misc/Error"
 
 const url = "http://localhost:5000/posts/"
 
 const Post = () => {
-    const path = useLocation().pathname.slice(3)
+    const history = useHistory()
+    const location = useLocation()
+    const path = location.pathname.slice(3)
 
     const [post, setPost] = useState(null)
 
@@ -18,15 +21,33 @@ const Post = () => {
         })
     }, [])
 
+    const handleLocationCheck = loc => {
+        if (location.pathname !== loc) history.push(loc)
+    }
+
     return (
         <div className="post-page-container">
             {
                 post ?
-                <>
-                    <div>
+                <div className="post-container">
+                    <div className="post-content-container">
+                        <div className="vote-container">
+                            <ImArrowUp/>
+                            <ImArrowDown/>
+                        </div>
+                        <div className="post-content">
+                            <p>
+                                r/{post.community} • Posted by
+                                <b onClick={() => handleLocationCheck(`/u/${post.creator}`)}> {post.creator}</b>
+                            </p>
+                            <h1>{post.title}</h1>
+                            <p style={{fontSize: "15px"}}>{post.context}</p>
+                        </div>
+                    </div>
+                    <div className="post-comment-container">
 
                     </div>
-                </> :
+                </div> :
                 (
                     post === null ? <Loading/> : <Error/>
                 )
